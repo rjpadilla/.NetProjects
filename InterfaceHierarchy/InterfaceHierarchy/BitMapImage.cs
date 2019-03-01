eb79e7435fdabbfde7fc5aba6068fd5381c0b70a
Raul Padilla <raulpadilla112@gmail.com>
2019-03-01 14:44:57 -0500

    Extend the functionality of an existing interface without breaking existing code bases.

---
+++ InterfaceHierarchy/InterfaceHierarchy/BitMapImage.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InterfaceHierarchy
+{
+    public class BitMapImage: IAdvancedDraw
+    {
+        public void Draw()
+        {
+            Console.WriteLine("Drawing...");
+        }
+
+        public void DrawInBoundingBox(int top, int left, int bottom, int right)
+        {
+            Console.WriteLine("Drawing in a box...");
+        }
+
+        public void DrawUpSideDown()
+        {
+            Console.WriteLine("Drawing upside down!");
+        }
+    }
+}
---
+++ InterfaceHierarchy/InterfaceHierarchy/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InterfaceHierarchy
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("***** Simple INterface Hierachy *****");
+
+            //Call from object level.
+            BitMapImage myBitmap = new BitMapImage();
+            myBitmap.Draw();
+            myBitmap.DrawInBoundingBox(10, 10, 100, 150);
+            myBitmap.DrawUpSideDown();
+
+            //Get IAdvancedDraw explicitly
+            IAdvancedDraw iAdvDraw = myBitmap as IAdvancedDraw;
+            if (iAdvDraw != null)
+                iAdvDraw.DrawUpSideDown();
+            Console.ReadLine();
+        }
+    }
+}
